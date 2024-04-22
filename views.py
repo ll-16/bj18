3c012202acdbf7a509b38745eae5b35469aefeea
ll-16 <pydone666777@163.com>
2024-04-22 11:20:15 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+form django.shortcuts import redirect 
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
